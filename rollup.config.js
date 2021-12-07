e4bf8aa61fba50827dfafcddfacb3231d1a1a0a9
Christoph Reiter <reiter.christoph@gmail.com>
2021-12-07 12:06:15 +0100

    rollup: clean up and add app config

--- rollup.config.js
+++ rollup.config.js
@@ -4,125 +4,104 @@ import glob from 'glob';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import copy from 'rollup-plugin-copy';
-import {terser} from "rollup-plugin-terser";
+import { terser } from "rollup-plugin-terser";
 import json from '@rollup/plugin-json';
 import serve from 'rollup-plugin-serve';
 import urlPlugin from "@rollup/plugin-url";
-import consts from 'rollup-plugin-consts';
 import license from 'rollup-plugin-license';
 import del from 'rollup-plugin-delete';
 import emitEJS from 'rollup-plugin-emit-ejs';
-import {getBabelOutputPlugin} from '@rollup/plugin-babel';
-import {getPackagePath, getBuildInfo, generateTLSConfig} from './vendor/toolkit/rollup.utils.js';
+import appConfig from './app.config.js';
+import { getBabelOutputPlugin } from '@rollup/plugin-babel';
+import { getPackagePath, getBuildInfo, generateTLSConfig, getDistPath } from './vendor/toolkit/rollup.utils.js';
 
-// -------------------------------
-
-// Some new web APIs are only available when HTTPS is active.
-// Note that this only works with a Non-HTTPS API endpoint with Chrome,
-// Firefox will emit CORS errors, see https://bugzilla.mozilla.org/show_bug.cgi?id=1488740
-const USE_HTTPS = true;
-
-// -------------------------------
-
+let appName = 'dbp-frontend-starter-app';
 const pkg = require('./package.json');
-const build = (typeof process.env.BUILD !== 'undefined') ? process.env.BUILD : 'local';
+const appEnv = (typeof process.env.APP_ENV !== 'undefined') ? process.env.APP_ENV : 'local';
 const watch = process.env.ROLLUP_WATCH === 'true';
-const buildFull = (!watch && build !== 'test') || (process.env.FORCE_FULL !== undefined);
-const matomoUrl = 'https://analytics.tugraz.at/';
-
-console.log("build: " + build);
-let basePath = '';
-let entryPointURL = '';
-let keyCloakServer = '';
-let keyCloakBaseURL = '';
-let keyCloakRealm = '';
-let keyCloakClientId = '';
-let matomoSiteId = 131;
+const buildFull = (!watch && appEnv !== 'test') || (process.env.FORCE_FULL !== undefined);
 let useTerser = buildFull;
 let useBabel = buildFull;
 let checkLicenses = buildFull;
+let useHTTPS = true;
 
-switch (build) {
-  case 'local':
-    basePath = '/dist/';
-    entryPointURL = 'http://127.0.0.1:8000';
-    keyCloakServer = 'auth-dev.tugraz.at';
-    keyCloakBaseURL = 'https://' + keyCloakServer + '/auth';
-    keyCloakClientId = 'auth-dev-mw-frontend-local';
-    keyCloakRealm = 'tugraz-vpu';
-    break;
-  case 'bs':
-    basePath = '/dist/';
-    entryPointURL = 'https://bs-local.com:8000';
-    keyCloakServer = 'auth-dev.tugraz.at';
-    keyCloakBaseURL = 'https://' + keyCloakServer + '/auth';
-    keyCloakClientId = 'auth-dev-mw-frontend-local';
-    keyCloakRealm = 'tugraz-vpu';
-    break;
-  case 'test':
-    break;
-  default:
-    console.error('Unknown build environment: ' + build);
+let config;
+if (appEnv in appConfig) {
+    config = appConfig[appEnv];
+} else {
+    console.error(`Unknown build environment: '${appEnv}', use one of '${Object.keys(appConfig)}'`);
     process.exit(1);
 }
 
+if (watch) {
+    config.basePath = '/dist/';
+}
+
+function getOrigin(url) {
+    if (url)
+        return new URL(url).origin;
+    return '';
+}
+
+config.CSP = `default-src 'self' 'unsafe-eval' 'unsafe-inline' \
+    ${getOrigin(config.matomoUrl)} ${getOrigin(config.keyCloakBaseURL)} ${getOrigin(config.entryPointURL)};\
+    img-src * blob: data:`;
+
 export default (async () => {
+    let privatePath = await getDistPath(pkg.name);
     return {
-        input: (build != 'test') ? glob.sync('src/*.js') : glob.sync('test/**/*.js'),
+        input: (appEnv != 'test') ? [
+            'src/' + appName + '.js',
+            'src/dbp-starter-activity.js',
+        ] : glob.sync('test/**/*.js'),
         output: {
-        dir: 'dist',
-        entryFileNames: '[name].js',
-        chunkFileNames: 'shared/[name].[hash].[format].js',
-        format: 'esm',
-        sourcemap: true
+            dir: 'dist',
+            entryFileNames: '[name].js',
+            chunkFileNames: 'shared/[name].[hash].[format].js',
+            format: 'esm',
+            sourcemap: true
         },
         preserveEntrySignatures: false,
-        // external: ['zlib', 'http', 'fs', 'https', 'url'],
         onwarn: function (warning, warn) {
             // ignore "suggestions" warning re "use strict"
             if (warning.code === 'MODULE_LEVEL_DIRECTIVE') {
                 return;
             }
             // ignore chai warnings
-            if (warning.code === 'CIRCULAR_DEPENDENCY') {
-            return;
+            if (warning.code === 'CIRCULAR_DEPENDENCY' && warning.message.includes('chai')) {
+                return;
             }
             // keycloak bundled code uses eval
             if (warning.code === 'EVAL') {
-            return;
+                return;
             }
             warn(warning);
         },
         plugins: [
             del({
-            targets: 'dist/*'
-            }),
-            consts({
-            environment: build,
-            buildinfo: getBuildInfo(build),
+                targets: 'dist/*'
             }),
             emitEJS({
-            src: 'assets',
-            include: ['**/*.ejs', '**/.*.ejs'],
-            data: {
-                getUrl: (p) => {
-                return url.resolve(basePath, p);
-                },
-                getPrivateUrl: (p) => {
-                    return url.resolve(`${basePath}local/${pkg.name}/`, p);
-                },
-                name: pkg.name,
-                entryPointURL: entryPointURL,
-                basePath: basePath,
-                keyCloakServer: keyCloakServer,
-                keyCloakBaseURL: keyCloakBaseURL,
-                keyCloakClientId: keyCloakClientId,
-                keyCloakRealm: keyCloakRealm,
-                environment: build,
-                matomoUrl: matomoUrl,
-                matomoSiteId: matomoSiteId,
-                buildInfo: getBuildInfo(build)
-            }
+                src: 'assets',
+                include: ['**/*.ejs', '**/.*.ejs'],
+                data: {
+                    getUrl: (p) => {
+                        return url.resolve(config.basePath, p);
+                    },
+                    getPrivateUrl: (p) => {
+                        return url.resolve(`${config.basePath}${privatePath}/`, p);
+                    },
+                    name: appName,
+                    entryPointURL: config.entryPointURL,
+                    basePath: config.basePath,
+                    keyCloakBaseURL: config.keyCloakBaseURL,
+                    keyCloakRealm: config.keyCloakRealm,
+                    keyCloakClientId: config.keyCloakClientId,
+                    CSP: config.CSP,
+                    matomoUrl: config.matomoUrl,
+                    matomoSiteId: config.matomoSiteId,
+                    buildInfo: getBuildInfo(appEnv)
+                }
             }),
             resolve({
                 // ignore node_modules from vendored packages
@@ -139,68 +118,66 @@ export default (async () => {
     <% _.forEach(dependencies, function (dependency) { if (dependency.name) { %>
     <%= dependency.name %>: <%= dependency.license %><% }}) %>
     `},
-            thirdParty: {
-                allow: {
-                test: '(MIT OR BSD-3-Clause OR Apache-2.0 OR LGPL-2.1-or-later OR 0BSD)',
-                failOnUnlicensed: true,
-                failOnViolation: true,
+                thirdParty: {
+                    allow: {
+                        test: '(MIT OR BSD-3-Clause OR Apache-2.0 OR LGPL-2.1-or-later OR 0BSD)',
+                        failOnUnlicensed: true,
+                        failOnViolation: true,
+                    },
                 },
-            },
             }),
             commonjs({
                 include: 'node_modules/**',
             }),
             json(),
             urlPlugin({
-            limit: 0,
-            include: [
-                "node_modules/suggestions/**/*.css",
-                "node_modules/select2/**/*.css",
-            ],
-            emitFiles: true,
-            fileName: 'shared/[name].[hash][extname]'
+                limit: 0,
+                include: [
+                    "node_modules/suggestions/**/*.css",
+                    "node_modules/select2/**/*.css",
+                ],
+                emitFiles: true,
+                fileName: 'shared/[name].[hash][extname]'
             }),
             copy({
                 targets: [
-                    {src: 'assets/silent-check-sso.html', dest:'dist'},
-                    {src: 'assets/htaccess-shared', dest: 'dist/shared/', rename: '.htaccess'},
-                    {src: 'assets/*.css', dest: 'dist/local/' + pkg.name},
-                    {src: 'assets/*.ico', dest: 'dist/local/' + pkg.name},
-                    {src: 'assets/*.svg', dest: 'dist/local/' + pkg.name},
-                    {src: 'assets/icon/*', dest: 'dist/local/'  + pkg.name + '/icon/'},
-                    {src: await getPackagePath('@dbp-toolkit/font-source-sans-pro', 'files/*'), dest: 'dist/local/' + pkg.name + '/fonts/source-sans-pro'},
-                    {src: await getPackagePath('@dbp-toolkit/common', 'src/spinner.js'), dest: 'dist/local/' + pkg.name, rename: 'spinner.js'},
-                    {src: await getPackagePath('@dbp-toolkit/common', 'misc/browser-check.js'), dest: 'dist/local/' + pkg.name, rename: 'browser-check.js'},
-                    {src: 'assets/icon-*.png', dest: 'dist/local/' + pkg.name},
-                    {src: 'assets/*-placeholder.png', dest: 'dist/local/' + pkg.name},
-                    {src: 'assets/manifest.json', dest: 'dist', rename: pkg.name + '.manifest.json'},
-                    {src: 'assets/*.metadata.json', dest: 'dist'},
-                    {src: await getPackagePath('@dbp-toolkit/common', 'assets/icons/*.svg'), dest: 'dist/local/@dbp-toolkit/common/icons'},
+                    { src: 'assets/silent-check-sso.html', dest: 'dist' },
+                    { src: 'assets/htaccess-shared', dest: 'dist/shared/', rename: '.htaccess' },
+                    { src: 'assets/*.css', dest: 'dist/' + await getDistPath(pkg.name) },
+                    { src: 'assets/*.ico', dest: 'dist/' + await getDistPath(pkg.name) },
+                    { src: 'assets/*.svg', dest: 'dist/' + await getDistPath(pkg.name) },
+                    { src: 'assets/icon/*', dest: 'dist/' + await getDistPath(pkg.name, 'icon') },
+                    { src: await getPackagePath('@dbp-toolkit/font-source-sans-pro', 'files/*'), dest: 'dist/' + await getDistPath(pkg.name, 'fonts/source-sans-pro') },
+                    { src: await getPackagePath('@dbp-toolkit/common', 'src/spinner.js'), dest: 'dist/' + await getDistPath(pkg.name) },
+                    { src: await getPackagePath('@dbp-toolkit/common', 'misc/browser-check.js'), dest: 'dist/' + await getDistPath(pkg.name) },
+                    { src: 'assets/manifest.json', dest: 'dist', rename: appName + '.manifest.json' },
+                    { src: 'assets/*.metadata.json', dest: 'dist' },
+                    { src: await getPackagePath('@dbp-toolkit/common', 'assets/icons/*.svg'), dest: 'dist/' + await getDistPath('@dbp-toolkit/common', 'icons') },
                 ],
             }),
             useBabel && getBabelOutputPlugin({
                 compact: false,
                 presets: [[
-                  '@babel/preset-env', {
-                    loose: true,
-                    modules: false,
-                    shippedProposals: true,
-                    bugfixes: true,
-                    targets: {
-                      esmodules: true
+                    '@babel/preset-env', {
+                        loose: true,
+                        modules: false,
+                        shippedProposals: true,
+                        bugfixes: true,
+                        targets: {
+                            esmodules: true
+                        }
                     }
-                  }
                 ]],
             }),
             useTerser ? terser() : false,
             watch ? serve({
-            contentBase: '.',
-            host: '127.0.0.1',
-            port: 8001,
-            historyApiFallback: basePath + pkg.name + '.html',
-            https: USE_HTTPS ? await generateTLSConfig() : false,
+                contentBase: '.',
+                host: '127.0.0.1',
+                port: 8001,
+                historyApiFallback: config.basePath + appName + '.html',
+                https: useHTTPS ? await generateTLSConfig() : false,
                 headers: {
-                    'Content-Security-Policy': `default-src 'self' 'unsafe-eval' 'unsafe-inline' analytics.tugraz.at eid.egiz.gv.at ${keyCloakServer} ${entryPointURL} httpbin.org ; img-src * blob: data:`
+                    'Content-Security-Policy': config.CSP
                 },
             }) : false
         ]
