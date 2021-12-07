bff040fe64f057300ed711f350cadc7771f62964
Christoph Reiter <reiter.christoph@gmail.com>
2021-12-07 10:43:58 +0100

    Remove unused script

--- scripts/vendor-update.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-###############################################################
-# Updates all vendor repositories to the latest master branch
-###############################################################
-
-git submodule foreach "git checkout master; git pull"
