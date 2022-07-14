2162244c8ec1b861e6a67c7a6d2de75dfed0e103
Gernot Leitgab <gernot@leitgab.at>
2022-07-14 05:44:31 +0200

    update unit test

--- test/unit.js
+++ test/unit.js
@@ -1,13 +1,13 @@
 import {assert} from 'chai';
 
-import '../src/dbp-mono-paymentmethod';
+import '../src/dbp-mono-processpayment';
 import '../src/dbp-mono.js';
 
 suite('dbp-template-activity basics', () => {
     let node;
 
     suiteSetup(async () => {
-        node = document.createElement('dbp-mono-paymentmethod');
+        node = document.createElement('dbp-mono-processpayment');
         document.body.appendChild(node);
         await node.updateComplete;
     });
