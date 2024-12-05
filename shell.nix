949e53824a8571293b7365ccf8ae248c494f0712
Patrizio Bekerle <patrizio@bekerle.com>
2024-12-05 14:29:19 +0100

    dev: add tooling
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

---
+++ shell.nix
@@ -0,0 +1,12 @@
+{ pkgs ? import <nixpkgs> {} }:
+  pkgs.mkShell {
+    # nativeBuildInputs is usually what you want -- tools you need to run
+    nativeBuildInputs = with pkgs; [
+      nodejs_22
+      curl
+      zellij # smart terminal workspace
+      lazygit # git terminal
+      just # task runner
+    ];
+}
+
