ea079a1dfaa22848b774743e9f99c21fddb15c7c
Christoph Reiter <reiter.christoph@gmail.com>
2025-05-22 11:10:47 +0200

    rollup: disable babel loose mode
    breaks dedupe mixin

--- rollup.config.js
+++ rollup.config.js
@@ -323,7 +323,7 @@ export default (async () => {
                         [
                             '@babel/preset-env',
                             {
-                                loose: true,
+                                loose: false,
                                 modules: false,
                                 shippedProposals: true,
                                 bugfixes: true,
